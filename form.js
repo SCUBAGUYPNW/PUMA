f217d8af598b199d61d4d215022fd409966fc2bc
Bill Odell <billodell@Bills-MacBook-Pro.local>
2017-11-09 15:23:37 -0800

    constructor working

--- form.js
+++ form.js
@@ -1,18 +1,18 @@
 'use strict';
 
-var observations = []
+var observations = [];
 function MakeInstance(id, gender, isYouth, box, landed, perched, roosting, foodcarrying, nestlingsobserved, nestlingsheard, juvenilereturns, comments) {
   this.id = id;
   this.gender = gender;
-  this.isYouth = false;
+  this.isYouth = isYouth;
   this.box = box;
-  this.landed = false;
-  this.perched = false;
-  this.roosting = false;
-  this.foodcarrying = false;
-  this.nestlingsobserved = false;
-  this.nestlingsheard = false;
-  this.juvenilereturns = false;
+  this.landed = landed;
+  this.perched = perched;
+  this.roosting = roosting;
+  this.foodcarrying = foodcarrying;
+  this.nestlingsobserved = nestlingsobserved;
+  this.nestlingsheard = nestlingsheard;
+  this.juvenilereturns = juvenilereturns;
   this.comments = comments;
   observations.push(this);
 }
@@ -22,18 +22,50 @@ function newInstanceSubmit(event) {
   console.log(event.target.elements);
   var id;
   var gender;
-  if ()
+  if (male.checked){
+    gender = 'Male';
+  }
+  if (female.checked){
+    gender = 'Female';
+  };
+  if (unsure.checked){
+    gender = 'Unsure';
+  }
   var isYouth;
-  var box;
+  if (isyouth.checked){
+    isYouth = true;
+  }
+  var box = boxnumber.value;
   var landed;
+  if (islanded.checked){
+    landed = true;
+  }
   var perched;
+  if (isperched.checked){
+    perched = true;
+  }
   var roosting;
+  if (isroosting.checked) {
+    roosting = true;
+  }
   var foodcarrying;
+  if (isfoodcarrying.checked) {
+    foodcarrying = true;
+  }
   var nestlingsobserved;
+  if (isnestlingsobserved.checked){
+    nestlingsobserved = true;
+  }
   var nestlingsheard;
+  if (isnestlingsheard.checked){
+    nestlingsheard = true;
+  }
   var juvenilereturns;
+  if (isjuvenilereturns.checked){
+    juvenilereturns = true;
+  }
   var comments;
 
-  // new MakeInstance ()
+  new MakeInstance (id, gender, isYouth, box, landed, perched, roosting, foodcarrying, nestlingsobserved, nestlingsheard, juvenilereturns, comments);
 };
 document.getElementById('puma-form').addEventListener('submit', newInstanceSubmit);
